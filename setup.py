470a88d53b19c4e890ea0dadacc29f1565871aea
andricDu <dusan.andric@oicr.on.ca>
2016-11-01 16:05:55 -0400

    1.0.1

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@
 from setuptools import setup, find_packages
 setup(
     name="SurvivalPy",
-    version="1.0.0",
+    version="1.0.1",
     packages=find_packages(),
 
     requires=["future", "scipy"],
